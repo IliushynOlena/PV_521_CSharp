3c64c69b16748d0a3da989d11bf8017f74eee22f
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2026-01-14 20:31:24 +0200

    Lambda function, delegates

--- 13_Delegates/Program.cs
+++ 13_Delegates/Program.cs
@@ -43,6 +43,28 @@
         }
 
     }
+    public delegate double CalcDelegate(double x, double y);
+    class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multy(double x, double y)
+        {
+            return x * y;
+        }
+        public double Div(double x, double y)
+        {
+            if(y != 0)
+                return x / y;
+            throw new DivideByZeroException();
+        }
+    }
 
     //class MyClass
     //{
@@ -62,10 +84,128 @@
     //    }
 
     //}
-    internal class Program
+    public delegate int ChangeDelegate(int value);
+    class Program
     {
+        public static void DoOperation(double a, double b, CalcDelegate c)
+        {
+            Console.WriteLine(c.Invoke(a,b));
+        }
+        static void ChangeEachElements(int[]array, ChangeDelegate action )
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                //array[i] = action(array[i]);
+                array[i] = action.Invoke(array[i]);
+            }
+        }
+        static int Sqrt(int v)
+        {
+            return v * v;
+        }
+        static int Increment(int v)
+        {
+            return ++v;
+        }
+        static int Decrement(int v)
+        {
+            return --v;
+        }
+
         static void Main(string[] args)
         {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10 };
+            foreach (var item in arr) Console.Write(item + " "); Console.WriteLine(); 
+            
+            ChangeEachElements(arr, Sqrt);
+            foreach (var item in arr) Console.Write(item+ " "); Console.WriteLine();
+
+            ChangeEachElements(arr, Increment);
+            foreach (var item in arr) Console.Write(item + " "); Console.WriteLine();
+
+            ChangeEachElements(arr, Decrement);
+            foreach (var item in arr) Console.Write(item + " "); Console.WriteLine();
+            //анонімний делегат
+            ChangeEachElements(arr, delegate(int value) { return --value; });
+            foreach (var item in arr) Console.Write(item + " "); Console.WriteLine();
+
+            //lambda function
+            //ChangeEachElements(arr,  (value) => { return --value; });
+            ChangeEachElements(arr,  (value) =>  --value);
+            foreach (var item in arr) Console.Write(item + " "); Console.WriteLine();
+
+            ChangeEachElements(arr, (value) => value+5);
+            foreach (var item in arr) Console.Write(item + " "); Console.WriteLine();
+
+            ChangeEachElements(arr, (value) => {
+
+                //some code
+                return value;
+            
+            });
+            foreach (var item in arr) Console.Write(item + " "); Console.WriteLine();
+            /*
+            Calculator calculator = new Calculator();
+            DoOperation(5, 2, calculator.Add);
+            DoOperation(5, 2, calculator.Sub);
+            DoOperation(5, 2, calculator.Multy);
+            DoOperation(5, 2, calculator.Div);
+            CalcDelegate calcDelegate = calculator.Add;
+            calcDelegate += calculator.Sub;
+            calcDelegate += calculator.Sub;
+            calcDelegate += calculator.Multy;
+            calcDelegate += calculator.Div;
+            calcDelegate -= calculator.Div;
+
+            foreach(Delegate  i in calcDelegate.GetInvocationList())
+            {
+                Console.WriteLine($"Res = { (i as CalcDelegate)?.Invoke(100,25)}");
+            }
+            */
+            /*
+            Calculator calculator = new Calculator();
+            double x, y;
+            int key;
+            do
+            {
+                CalcDelegate calcDelegate = null;
+                Console.WriteLine("Enter first number ");
+                x = double.Parse(Console.ReadLine()!);
+                Console.WriteLine("Enter second number ");
+                y = double.Parse(Console.ReadLine()!);
+                Console.WriteLine("Add  - 1 ");
+                Console.WriteLine("Sub  - 2 ");
+                Console.WriteLine("Mult  - 3 ");
+                Console.WriteLine("Divide  - 4 ");
+                Console.WriteLine("Exit  - 0 ");
+                key = int.Parse(Console.ReadLine()!);
+                switch (key)
+                {
+                    case 1:
+                        calcDelegate = new CalcDelegate(calculator.Add);
+                        //calculator.Add(x, y);
+                        break;
+                    case 2:
+                        calcDelegate = new CalcDelegate(calculator.Sub);
+                        break;
+                    case 3:
+                        calcDelegate = calculator.Multy;
+                        break;
+                    case 4:
+                        calcDelegate = calculator.Div;
+                        break;
+                    case 0:
+                        Console.WriteLine("Good  Buy");
+                        break;
+                    default:
+                        Console.WriteLine("Error choice......");
+                        break;
+                }
+                Console.WriteLine($"Res =  {calcDelegate?.Invoke(x,y)}");
+
+            } while (key != 0);
+            */
+            /*
             //MyClass myClass = new MyClass("Olga");  
             
             GetDoubleDelegate getDouble =  new GetDoubleDelegate(SuperClass.GetKoef);
@@ -101,7 +241,7 @@
             {
                 (item as VoidDelegate)?.Invoke();
             }
-
+            */
         }
     }
 }
