797bd18befbb45f83e9bd5d12e93fc41e16841f3
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2026-01-19 20:07:08 +0200

    Dictionary, GB

---
+++ 16_GarbageCollector/Program.cs
@@ -0,0 +1,163 @@
+﻿using System.Net.Sockets;
+using System.Security.Cryptography;
+
+namespace _16_GarbageCollector
+{
+    class Person
+    {
+        string name;
+        string surname;
+        int age;
+    }
+    class GarbageHELPER
+    {
+        public void MakeGarbage()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Person person = new Person();
+            }
+        }
+       
+    }
+    class TestClass
+    {
+        public int MyProperty { get; set; }
+        public TestClass()
+        {
+            Console.WriteLine("Constructor");
+        }
+        ~TestClass()
+        {
+            Console.WriteLine("Finalize!");
+        }
+    }
+    class DisposeExample : IDisposable
+    {
+        Stream stream;
+        public DisposeExample()
+        {
+            Console.WriteLine("Open stream....");
+            stream = new FileStream(@"C:\Test\Csharp\doc.txt", FileMode.OpenOrCreate);
+        }
+        public void Dispose()
+        {
+            stream.Close();
+            Console.WriteLine("Close connection");
+            //забороняє Garbage collectory запускати фіналізатора
+            GC.SuppressFinalize(this);
+        }
+        public void DoSomething()
+        {
+            throw new Exception();
+        }
+        ~DisposeExample()
+        {
+            stream.Close();
+            Console.WriteLine("Close connection");
+        }
+    }
+    class Test : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Close resourses");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkStream
+            TestClass testClass = new TestClass();
+
+            if (true)
+            {
+                DisposeExample disposeExample = new DisposeExample();
+                // throw exception....
+                try
+                {
+                    disposeExample.DoSomething();
+                }
+                catch (Exception ex)
+                {
+                    //work with object....
+                    disposeExample.Dispose();
+                }
+                finally
+                {
+                    disposeExample.Dispose();
+                }
+
+                using (Test t = new Test())
+                {
+                    t.SomeMethod();
+                    //throw ex
+
+
+                   
+                }//t.Dispose( t.Close())
+
+                using (DisposeExample d = new DisposeExample())
+                {
+                    d.DoSomething();
+
+                }// d.Dispose();
+
+
+                DisposeExample d = new DisposeExample();
+                using (FileStream w = new FileStream())
+                {
+                    w.Write();
+
+                }// w.Dispose();
+
+
+                using (string s = new string("hello"))
+                {
+                    
+
+                }// w.Dispose();
+
+
+            }
+           
+            //GarbageHELPER gc = new GarbageHELPER();
+            //Console.WriteLine(GC.GetGeneration(gc));
+            //Console.WriteLine($"Total memory : {GC.GetTotalMemory(false)}");
+            //gc.MakeGarbage();
+            //Console.WriteLine($"Total memory : {GC.GetTotalMemory(false)}");
+            //GC.Collect(0);
+            //Console.WriteLine(GC.GetGeneration(gc));
+            //Console.WriteLine($"Total memory : {GC.GetTotalMemory(false)}");
+            //GC.Collect(1);
+            //Console.WriteLine(GC.GetGeneration(gc));
+            //Console.WriteLine($"Total memory : {GC.GetTotalMemory(false)}");
+
+
+
+            //int a = 5;
+            //string str = "1";
+            //string str3;
+            //if (true)
+            //{
+            //    string copy;
+
+            //    if (true)
+            //    {
+            //        int b = 10;
+            //        string str2 = "hello";
+            //        copy = str2;
+            //        Console.WriteLine(str2);
+            //    }
+            //    Console.WriteLine(copy);
+            //    str3 = copy;
+
+            //}
+
+
+
+
+        }
+    }
+}
---
+++ 17_Dictionary/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace _17_Dictionary
+{
+    class Person
+    {
+        public string  Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string>countries = new Dictionary<string, string>();
+
+            countries.Add("UA", "Ukraine");
+            countries.Add("PL", "Poland");
+            countries.Add("FR", "France");
+            countries.Add("CH", "China");
+            countries.Add("USA", "United States");
+
+            foreach (KeyValuePair<string, string> country in countries)
+            {
+                Console.WriteLine(country.Key + " - " + country.Value);
+            }
+
+            string c = countries["UA"];
+            Console.WriteLine(c);
+
+            countries["USA"] = "America";
+            Console.WriteLine(countries["USA"]);
+
+            countries["IN"] = "India";
+            Console.WriteLine("---------------------------");
+            foreach (KeyValuePair<string, string> country in countries)
+            {
+                Console.WriteLine(country.Key + " - " + country.Value);
+            }
+
+            Dictionary<char, Person> people = new Dictionary<char, Person>();
+            people.Add('b', new Person() { Name = "Bill" });
+            people.Add('t', new Person() { Name = "Tom" });
+            people.Add('j', new Person() { Name = "Jack" });
+            people.Add('r', new Person() { Name = "Rita" });
+            foreach (var item in people)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
+            Console.WriteLine("-------------------------------");
+
+            people['b'] = new Person() { Name = "Bob" };
+
+            if(people.ContainsKey('a'))
+            {
+                people['a'] = new Person() { Name = "Anna" };
+            }
+            else
+            {
+                Console.WriteLine("Does not key.");
+            }
+
+                foreach (var item in people)
+                {
+                    Console.WriteLine(item.Key + " - " + item.Value);
+                }
+            Console.WriteLine("-------Keys---------------");
+            foreach (var key in people.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            Console.WriteLine("-------Values---------------");
+            foreach (var value in people.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            people.Remove('b');
+            foreach (var item in people)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
+        }
+    }
+}
