a7422e4eae724cd5dcaba3582b52620d763e9f3e
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2026-01-21 19:56:38 +0200

    Generic collections

---
+++ 18_Collections/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+
+namespace _18_Collections
+{
+    class MyClass
+    {
+        public MyClass(int numbers)
+        {
+            
+        }
+        public static implicit operator int(MyClass cl)
+        {
+            return 10;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] {1,2,3,4,5};
+
+            ArrayList arr1 = new ArrayList();
+            Console.WriteLine(arr1.Capacity);
+            Console.WriteLine(arr1.Count);
+
+            ArrayList arr2 = new ArrayList(5);
+            Console.WriteLine(arr2.Capacity);
+            Console.WriteLine(arr2.Count);
+
+            ArrayList arr3 = new ArrayList(new int[] { 1, 2, 3, 4, 6 });
+            Console.WriteLine(arr3.Capacity);
+            Console.WriteLine(arr3.Count);
+
+            arr3.Add(100);
+            arr3.Add("one");
+            arr3.Add("two");
+            arr3.Add(false);
+            arr3.AddRange(new char[] { 'a', 'b', 'c','d' });
+
+            Console.WriteLine(arr3.Capacity);
+            Console.WriteLine(arr3.Count);
+
+            foreach (object item in arr3)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            arr3.Insert(2, "Hello world");
+
+            arr3.RemoveAt(0);
+          
+            foreach (object item in arr3)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            //arr3.Sort();
+            foreach (object item in arr3)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+
+            Stack stack = new Stack();
+            stack.Push(100);
+            stack.Push(200);
+            stack.Push(300);
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine("----------------------");
+            stack.Push("blue");
+            stack.Push("green");
+            stack.Push("yellow");
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(stack.Contains("one"));
+            Console.WriteLine(stack.Contains("green"));
+
+            //boxing -- unboxing
+            // 4 --> int --> value   --> Stack
+            //value ---> references - boxing
+            //references ---> value   --> unboxing
+
+            object obj = new object();// references --- > references
+            int number = 5;//value --> value
+
+            //boxing
+            obj = (object) number;
+
+            //unboxing
+            number = (int) obj;
+
+            //boxing
+            MyClass myClass = new MyClass(15);
+
+            //unboxing
+            int res =(int) myClass;
+
+
+        }
+    }
+}
---
+++ 19_Generics/Program.cs
@@ -0,0 +1,195 @@
+﻿namespace _19_Generics
+{
+    /*
+     Generics :
+    - collection
+    - class
+    - struct
+    - methods
+    - Interfaces
+    - delegate
+
+     
+     */
+
+    //Generic Delegates
+    //template<typename Type>
+    public delegate Type SummaDelegate<Type>(Type x, Type y);
+    public delegate Type SubDelegate<Type>(Type x, Type y);
+    public delegate bool CompareDelegate<T1,T2>(T1 x, T2 y);
+
+    //Generic Interface
+    interface IMyComparable<T>
+    {
+        void CompareMyObject(T obj1, T obj2);
+    }
+    class User : IMyComparable<User>
+    {
+        public void CompareMyObject(User obj1, User obj2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    interface IIndexer<T>
+    {
+        T this[int index] { get; set; }
+    }
+    //Generic Class
+    //template<typename Type>
+    class MyArray<Type> : IMyComparable<MyArray<Type>>, IIndexer<Type>
+    {
+        private Type[] arr;
+
+        public MyArray(int size = 10)
+        {
+            Random random = new Random();
+            arr = new Type[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = default(Type)!;
+            }
+        }
+        public void AddElement(Type el)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = el;
+        }
+        public override string ToString()
+        {
+            string res = " ";
+            foreach (var item in arr)
+            {
+                res += item + " ";
+            }
+            return res;
+        }
+        public Type this[int index] {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+
+        public void CompareMyObject(MyArray<Type> obj1, MyArray<Type> obj2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Abonent
+    {
+        public Abonent(string name, string phone)
+        {
+            
+        }
+    }
+    /*
+     *  where T1 : class, new ()
+        struct -  type is struct
+        class -  type is class
+        interface -  type is interface
+        new() - type has default constructor
+        BaseClass -  type is BasseClass
+     
+     */
+    class Point<T1, T2> //where T1 : class, new ()
+    {
+        public T1 X { get; set; }
+        public T2 Y { get; set; }
+        public Point()
+        {
+            X = default(T1)!;
+            Y = default(T2)!;
+
+
+            //T1 value = new T1();
+        }
+        public Point(T1 x, T2 y)
+        {
+          X = x; Y = y;
+            if( typeof(T1) == typeof(int))
+                Console.WriteLine("T1 is int!");
+        }
+        public override string ToString()
+        {
+            return $"X : {X}. Y : {Y}";
+        }
+
+    }
+    internal class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static float Add(float a, float b)
+        {
+            return a + b;
+        }
+        static float Sub(float a, float b)
+        {
+            return a - b;
+        }
+        static bool Compare(double d1, decimal d2)
+        {
+            return (decimal)d1 == d2;
+        }
+        //Generics methods
+        static void ShowObject<T>(T obj)
+        {
+            Console.WriteLine(obj);
+        }
+        static bool CompareElements<T1, T2>(T1 obj1, T2 obj2)
+        {
+            return obj1.GetHashCode() == obj2.GetHashCode();
+        }
+        static T3 Multy<T1,T2,T3>(T1 a,T2 b)
+        {
+            //some action  res T3
+            return default(T3);
+        }
+        static void Main(string[] args)
+        {
+            List<int> list;
+            Dictionary<string, string> dict;
+            Stack<int> s;
+            Queue<float> q;
+            Multy<int, float, double>(5, 7);
+
+            short a = 5;
+            int b = 10;
+            if(CompareElements<short, int>(a,b))
+                Console.WriteLine("Hash code are equals");
+
+            ShowObject(5);
+            ShowObject("one");
+            ShowObject(false);
+
+            SummaDelegate<int> summa = new SummaDelegate<int>(Add);
+            Console.WriteLine(summa.Invoke(15, 4)); ;
+            SubDelegate<float> sub = new SubDelegate<float>(Sub);
+            Console.WriteLine(sub.Invoke(100, 80)); ;
+
+            CompareDelegate<double, decimal> compare = new CompareDelegate<double, decimal>(Compare);
+            Console.WriteLine(compare.Invoke(10.5, 3.4m)); ;
+
+            MyArray<float> my = new MyArray<float>();
+            Console.WriteLine(my);
+            my.AddElement(8.25f);
+            my.AddElement(3.3f);
+            Console.WriteLine(my);
+
+            MyArray<bool> myBool = new MyArray<bool>();
+            Console.WriteLine(myBool);
+            myBool.AddElement(true);
+            myBool.AddElement(true);
+            Console.WriteLine(myBool);
+
+            Point<int, double> point = new Point<int, double>(5, 4.7);
+            Console.WriteLine(point);
+        }
+    }
+}
